88181c2af9d7ee526398e86f98e46217927b0305
м <174154109+n11kol11c@users.noreply.github.com>
2025-12-05 12:30:07 +0100

    Update zadatak5.php

--- razvoj_veb_aplikacija_1_zadaci/zadatak5.php
+++ razvoj_veb_aplikacija_1_zadaci/zadatak5.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Zadatak 4 - Razvoj Veb Aplikacija</title>
+    <title>Zadatak 5 - Razvoj Veb Aplikacija</title>
 </head>
 <body>
     <?php
