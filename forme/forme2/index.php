9782061912c735d0c4f2f9dec474ffb4a97872ad
м <174154109+n11kol11c@users.noreply.github.com>
2026-01-23 14:43:56 +0100

    Add HTML form in index.php

---
+++ forme/forme2/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forma</title>
+</head>
+<body>
+    <form action="forma/forma.php" method="POST" name="forma">
+        <label for="ime">Ime: </label><input type="text" name="ime" checked>
+        <input type="submit">
+    </form>
+</body>
+</html>
