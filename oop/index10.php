e571abb8717ad4ac86ebaf24631a95e8d88d5ee0
м <174154109+n11kol11c@users.noreply.github.com>
2026-03-25 23:44:52 +0100

    Create index10.php

---
+++ oop/index10.php
@@ -0,0 +1,34 @@
+<?php
+interface Narudzbina {
+    public function izracunaj(): float;
+}
+
+abstract class Proizvod {
+    protected string $naziv;
+    protected float $osnovnaCena;
+
+    public function __construct(string $naziv, float $osnovnaCena) {
+        $this->naziv = $naziv;
+        $this->osnovnaCena = $osnovnaCena;
+    }
+}
+
+class Hrana extends Proizvod implements Narudzbina {
+    private static float $popust = 0.10;
+
+    public function __construct(string $naziv, float $osnovnaCena) {
+        parent::__construct($naziv, $osnovnaCena);
+    }
+
+    public function izracunaj(): float {
+        return $this->osnovnaCena * (1 - self::$popust);
+    }
+
+    public function __toString(): string {
+        return "Hrana: {$this->naziv}, Cena sa popustom: " . $this->izracunaj() . "e";
+    }
+}
+
+$piletina = new Hrana("Piletina ispod sača", 12.00);
+echo $piletina;
+?>
